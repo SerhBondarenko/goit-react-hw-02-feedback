dcf3795df2106d8ae5ac03545265c1c7cf10c537
SerhBondarenko <serhii.bondarenk@gmail.com>
2022-02-06 17:08:31 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Feedback from '../src/components/Feedback/Feedback'
+
+function App() {
+  return (
+    <Feedback good={0} neutral={0} bad={0}/>
+  );
+}
+
+export default App;
